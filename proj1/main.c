e306e173e6954a098279d1606ef1e10487bb7aef
Patilhas <joao.campos1221@gmail.com>
2019-03-18 18:54:16 +0000

    Initial version

---
+++ proj1/main.c
@@ -0,0 +1,31 @@
+/*
+ *  Operative Systems Project
+ *  by Joao Campos
+ *     Diogo Sousa
+ *     José Martins 
+ * 
+ *  18/03/2019 
+ */
+
+
+/* Objetivos:
+ * Percorrer sistema de ficheiros
+ * Recolher/Criar informação sobre ficheiros
+ * Apresentar a informação
+ * Utilizador escolhe o diretório/ficheiro a ser lido 
+ *                    o ficheiro onde é colocada a informação
+ *                    o local de geração de log files
+ * Interrupção da execução a comando do utilizador
+ */
+
+/* foresinc
+ * [-r]   analisar todos os ficheiros do diretório e subdiretorios
+ * [-h]   calcular has dos ficheiros analisados
+ * 
+ */
+
+
+
+int main(){
+    return 0;
+}
