fc22f5c414bb66970686a51bb97d56b4143fa9bd
Patilhas <joao.campos1221@gmail.com>
2019-03-18 19:58:38 +0000

    Added exit on wrong number of arguments

--- proj1/main.c
+++ proj1/main.c
@@ -2,17 +2,16 @@
  *  Operative Systems Project
  *  by Joao Campos
  *     Diogo Sousa
- *     José Martins 
- * 
- *  18/03/2019 
+ *     José Martins
+ *
+ *  18/03/2019
  */
-
 
 /* Objetivos:
  * Percorrer sistema de ficheiros
  * Recolher/Criar informação sobre ficheiros
  * Apresentar a informação
- * Utilizador escolhe o diretório/ficheiro a ser lido 
+ * Utilizador escolhe o diretório/ficheiro a ser lido
  *                    o ficheiro onde é colocada a informação
  *                    o local de geração de log files
  * Interrupção da execução a comando do utilizador
@@ -21,11 +20,35 @@
 /* foresinc
  * [-r]   analisar todos os ficheiros do diretório e subdiretorios
  * [-h]   calcular has dos ficheiros analisados
- * 
+ * [-o]   guardar dados no ficheiro
+ * [-v]   gerar log files
  */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
+int main(int argc, char** argv) {
+  switch (argc) {
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    default:
+      if (argc < 2) {
+        write(STDOUT_FILENO, "Not enough arguments.\n", 22);
+      } else {
+        write(STDOUT_FILENO, "Too many arguments.\n", 20);
+      }
 
-int main(){
-    return 0;
+      _exit(argc);
+  }
+
+  return 0;
 }
