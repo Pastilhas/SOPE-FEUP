5064819cebf887486e5a6445df76b64c8bc6c924
Patilhas <joao.campos1221@gmail.com>
2019-03-27 21:30:45 +0000

    Cleanup and can foresinc files.

--- proj1/main.c
+++ proj1/main.c
@@ -25,6 +25,7 @@
 
 #include <dirent.h>
 #include <fcntl.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,29 +36,10 @@
 void dir_info(char* dirname);
 void file_info(char* filename, char* d_name);
 void get_stat(struct stat* info, char* filename, char* d_name);
+int isDir(char* line);
+int isRoot(char* line);
 
 int main(int argc, char** argv) {
-  // forensic file
-
-  // forensic -r [file]
-  // forensic -v [file]
-
-  // forensic -r -v [file]
-  // forensic -h [type] [file]
-  // forensic -o [file] [file]
-
-  // forensic -r -h [type] [file]
-  // forensic -h [type] -v [file]
-  // forensic -r -o [file] [file]
-  // forensic -o [file] -h [file]
-
-  // forensic -r -h [type] -v [file]
-  // forensic -r -o [file] -v [file]
-  // forensic -h [type] -o [file] [file]
-
-  // forensic -r -h [type] -o [file] [file]
-  // forensic -h [type] -o [file] -v [file]
-
   // forensic -r -h [type] -o [file] -v [file]
 
   if (argc < 2 || argc > 8) {
@@ -70,7 +52,22 @@ int main(int argc, char** argv) {
   }
 
   char* log_name = getenv("LOGFILENAME");
-  char* file_name = argv[argc - 1];
+  char* file_name;
+
+  if (isRoot(argv[argc - 1])) {
+    file_name = (char*)malloc(strlen(argv[argc - 1]) + 1);
+    strcpy(file_name, argv[argc - 1]);
+  } else {
+    file_name = (char*)malloc(strlen(argv[argc - 1]) + 3);
+    strcpy(file_name, "./");
+    strcat(file_name, argv[argc - 1]);
+  }
+
+  if (isDir(file_name)) {
+    dir_info(file_name);
+  } else {
+    file_info(file_name, basename(file_name));
+  }
 
   return 0;
 }
@@ -81,37 +78,58 @@ void dir_info(char* dirname) {
 
   if (dir_ptr == NULL) {  // Fail to open directory
     write(STDOUT_FILENO, "Cannot open.\n", 13);
+
   } else {
     dirent = readdir(dir_ptr);
     while (dirent != NULL) {
-      // Allocate memory for file path
-      char* filename =
-          (char*)malloc(strlen(dirname) + strlen(dirent->d_name) + 2);
-      if (filename != NULL) {
-        // Create path to file   dir/filename
-        strcpy(filename, dirname);
-        strcat(filename, "/");
-        strcat(filename, dirent->d_name);
-        file_info(filename, dirent->d_name);
-        free(filename);
-      } else {
-        write(STDOUT_FILENO, "Failed to create file path.\n", 28);
-        exit(-1);
+      if (dirent->d_name[0] != '.') {
+        // Allocate memory for file path
+        char* filename =
+            (char*)malloc(strlen(dirname) + strlen(dirent->d_name) + 2);
+        if (filename != NULL) {
+          // Create path to file   dir/filename
+          strcpy(filename, dirname);
+          strcat(filename, "/");
+          strcat(filename, dirent->d_name);
+          file_info(filename, dirent->d_name);
+          free(filename);
+        } else {
+          write(STDOUT_FILENO, "Failed to create file path.\n", 28);
+          exit(-1);
+        }
       }
 
       dirent = readdir(dir_ptr);
     }
   }
+
   closedir(dir_ptr);
 }
 
 void file_info(char* filename, char* d_name) {
   struct stat info;
   get_stat(&info, filename, d_name);
+
+  char* name = filename;
+  int size = (int)info.st_size;
+  
+  printf("%s,%d\n", d_name, size);
 }
 
 void get_stat(struct stat* info, char* filename, char* d_name) {
-  if (stat(d_name, info)) {
-    perror(filename);
+  if (stat(filename, info)) {
+    perror(d_name);
   }
+}
+
+int isRoot(char* line) {
+  if (line[0] == '/') return 1;
+  return 0;
+}
+
+int isDir(char* line) {
+  struct stat info;
+  get_stat(&info, line, line);
+
+  return S_ISDIR(info.st_mode);
 }
