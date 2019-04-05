22683cf8d144bbb9c4d006d313952b488691dfaf
Pastilhas <joao.campos1221@gmail.com>
2019-04-05 20:12:04 +0000

    Last Commit I hope

---
+++ getters.h
@@ -0,0 +1,28 @@
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define READ 0
+#define WRITE 1
+
+#define COMMAND_LOG 0
+#define FILE_LOG 1
+
+void get_stat(struct stat *info, char *filename, char *d_name);
+int isDir(char *line);
+int isRoot(char *line);
+char *getAccess(mode_t mode);
+void getDate(char *str, time_t date);
+char *getFileType(char *path);
+char *getMD5(char *path);
+char *getSHA1(char *path);
+char *getSHA256(char *path);
